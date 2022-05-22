d727ba36d1a8fc58911df7c2fd7be4db9eda3bf8
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-22 09:20:22 +0300

    0x11. C - printf task 0 - 3 commit

--- _printf_bin_oct.c
+++ _printf_bin_oct.c
@@ -53,7 +53,7 @@ unsigned int printf_b(va_list val, int counter)
  * Return: # of characters printed.
  */
 
-unsigned int printf_0(va_list val, int counter)
+unsigned int printf_f(va_list val, int counter)
 {
 	long int octal = va_arg(val, unsigned int);
 	unsigned int tmp;
