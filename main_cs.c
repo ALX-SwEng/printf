9909e2743119f15e6291d58d3c94883d0ccf90e0
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-22 09:26:16 +0300

    0x11. C - printf task 0 - 3 commit

---
+++ main_cs.c
@@ -0,0 +1,76 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success, error code otherwise
+ */
+ /*
+int main(void)
+{
+	int len, len2;
+
+	len = _printf("Let's print a simple sentence.\n");
+	len2 = printf("Let's print a simple sentence.\n");
+
+	_printf("%c\n", 'S');
+	printf("%c\n\n", 'S');
+	
+	_printf("A char inside a sentence: %c. Did it work?\n", 'F');
+	printf("A char inside a sentence: %c. Did it work?\n\n", 'F');	
+	
+	_printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
+	printf("Let'see if the cast is correctly done: %c. Did it work?\n\n", 48);
+	
+	_printf("%s", "This sentence is retrieved from va_args!\n");
+	printf("%s", "This sentence is retrieved from va_args!\n\n");
+	
+	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
+	printf("Complete the sentence: You %s nothing, Jon Snow.\n\n", "know");
+	
+	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0); 
+	printf("Complete the sentence: You %s nothing, Jon Snow.\n\n", (char *)0);
+	
+	_printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 
+		'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+	printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 
+		'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n', '\n');
+
+	_printf("%%\n");
+	printf("%%\n\n");
+	_printf("Should print a single percent sign: %%\n");
+	printf("Should print a single percent sign: %%\n\n");
+	
+	_printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
+	printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n', '\n');
+	_printf("css%ccs%scscscs\n", 'T', "Test");
+	printf("css%ccs%scscscs\n\n", 'T', "Test");
+	
+	_printf(str); 
+	_printf("man gcc:\n%s", str);
+	
+	_printf(NULL);
+	printf(NULL);
+	
+	_printf("%c\n", '\0');
+	printf("%c\n\n", '\0');
+	
+	_printf("%\n");
+	printf("%\n\n");
+	
+	_printf("%!\n");
+	printf("%!\n\n");
+	
+	_printf("%K\n");
+	printf("%K\n");
+	
+	fflush(stdout);
+	if (len != len2)
+	{
+		printf("Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+	return (0);
+}
+*/
