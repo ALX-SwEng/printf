890641577b94ad99eaee41dcf87c9b9f38b9240c
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-22 09:53:35 +0300

    0x11. C - printf task 0 - 3 commit

--- main.c
+++ main.c
@@ -12,23 +12,23 @@ int main(void)
 	int len, len2;
 	long res = INT_MAX;
 
-	len = _printf("%i", 10000);
-	len2 = printf("%i\n\n", 10000);
+	len = _printf("%i\n", 10000);
+	len2 = printf("%i\n", 10000);
 	fflush(stdout);
 	if (len != len2)
 	{
-		printf("Lengths differ.\n\n");
+		printf("Lengths differ1.\n\n");
 		fflush(stdout);
 		return (1);
 	}
 
 	res *= 2;
 	len = _printf("%b + %b = %b\n", INT_MAX, INT_MAX, res);
-	len2 = printf("1111111111111111111111111111111 + 1111111111111111111111111111111 = 11111111111111111111111111111110\n\n");
+	len2 = printf("1111111111111111111111111111111 + 1111111111111111111111111111111 = 11111111111111111111111111111110\n");
 	fflush(stdout);
 	if (len != len2)
 	{
-		printf("Lengths differ.\n");
+		printf("Lengths differ2.\n");
 		fflush(stdout);
 		return (1);
 	}
