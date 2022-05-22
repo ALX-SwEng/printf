05329b550fa9a3a42afad1e0e84fc60271842b0e
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-22 09:39:15 +0300

    0x11. C - printf task 0 - 3 commit

--- main.c
+++ main.c
@@ -14,26 +14,12 @@ int main(void)
 	len2 = printf("Let's print a simple sentence.\n");
 
 	_printf("%c\n", 'S');
-	printf("%c\n\n", 'S');
-	
 	_printf("A char inside a sentence: %c. Did it work?\n", 'F');
-	printf("A char inside a sentence: %c. Did it work?\n\n", 'F');	
-	
-	_printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
-	printf("Let'see if the cast is correctly done: %c. Did it work?\n\n", 48);
-	
+	_printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);	
 	_printf("%s", "This sentence is retrieved from va_args!\n");
-	printf("%s", "This sentence is retrieved from va_args!\n\n");
-	
 	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
-	printf("Complete the sentence: You %s nothing, Jon Snow.\n\n", "know");
-	
-	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0); 
-	printf("Complete the sentence: You %s nothing, Jon Snow.\n\n", (char *)0);
-	
+	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);	
 	_printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
-	printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c\n", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
-
 	_printf("%%\n");
 	printf("%%\n\n");
 	_printf("Should print a single percent sign: %%\n");
