bf6b2542f2b6e97b8913b8e84978cab03a36536c
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-22 09:47:27 +0300

    0x11. C - printf task 0 - 3 commit

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -5,32 +7,12 @@
  *
  * Return: 0 on success, error code otherwise
  */
- 
 int main(void)
 {
 	int len, len2;
 
-	len = _printf("Let's print a simple sentence.\n");
-	len2 = printf("Let's print a simple sentence.\n");
-
-	_printf("%c\n", 'S');
-	_printf("A char inside a sentence: %c. Did it work?\n", 'F');
-	_printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);	
-	_printf("%s", "This sentence is retrieved from va_args!\n");
-	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
-	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);	
-	_printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
-	_printf("%%\n");
-	_printf("Should print a single percent sign: %%\n");	
-	_printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
-	_printf("css%ccs%scscscs\n", 'T', "Test");
-	
-	_printf(NULL);
-	_printf("%c\n", '\0');
-	_printf("%\n");
-	_printf("%!\n");	
-	_printf("%K\n");
-	
+	len = _printf("%i", 10000);
+	len2 = printf("%i", 10000);
 	fflush(stdout);
 	if (len != len2)
 	{
