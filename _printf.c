e6082e40a275a335ea353e9c5763735775136760
Chere Lemma <chere.lemma@aastu.edu.et>
2022-08-07 16:17:48 +0300

    printf project update commit

--- _printf.c
+++ _printf.c
@@ -1,103 +1,68 @@
 #include "main.h"
-
+void cleanup(va_list args, buffer_t *output);
+int run_printf(const char *format, va_list args, buffer_t *output);
+int _printf(const char *format, ...);
 /**
- * _printf - print a formatted output.
- * @format: string pointer to print - may contain directives
- * Return: # of characters printed.
+ * cleanup - Peforms cleanup operations for _printf.
+ * @args: A va_list of arguments provided to _printf.
+ * @output: A buffer_t struct.
  */
-
-int _printf(const char *format, ...)
+void cleanup(va_list args, buffer_t *output)
 {
-	va_list varAargs;
-	int counter;
-
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-
-	va_start(varAargs, format);
-	counter = printf_output(format, varAargs);
-	va_end(varAargs);
-
-return (counter);
-}
-
+va_end(args);
+write(1, output->start, output->len);
+free_buffer(output); }
 /**
- * printf_output - print a formatted output.
- * @format: string pointer to print - may contain directives.
- * @val: Variable number of arguments.
- * Return: # of characters printed.
- */
-int printf_output(const char *format, va_list val)
-{
-	int i = 0, counter = 0;
-	unsigned int (*f)(va_list, int);
-
-	while (format[i])
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == '\0')
-			{
-				_putchar(format[i - 1]);
-				++counter;
-			}
-
-			else
-			{
-				i++;
-				f = check_specifiers(format + i);
-				if (f == NULL)
-				{
-					if (format[i] != '%')
-					{
-						_putchar(format[i - 1]);
-						++counter;
-					}
-
-					_putchar(format[i]);
-					++counter;
-				}
-				else
-					counter += f(val, 0);
-			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			++counter;
-		}
-		++i;
-	}
-return (counter);
-}
-
-/**
- * check_specifiers - Matches a specifier
- *					with a corresponding print function.
- * @specifier: A pointer to a potential specifier.
+ * run_printf - Reads through the format string for _printf.
+ * @format: Character string to print - may contain directives.
+ * @output: A buffer_t struct containing a buffer.
+ * @args: A va_list of arguments.
  *
- * Return: a pointer to the function if specifier is matched,
- *         Otherwise - NULL.
+ * Return: The number of characters stored to output.
  */
-
-unsigned int (*check_specifiers(const char *specifier))(va_list, int)
+int run_printf(const char *format, va_list args, buffer_t *output)
+{ int i, wid, prec, ret = 0;
+char tmp;
+unsigned char flags, len;
+unsigned int (*f)(va_list, buffer_t *,
+unsigned char, int, int, unsigned char);
+for (i = 0; *(format + i); i++)
+{ len = 0;
+if (*(format + i) == '%')
+{ tmp = 0;
+flags = handle_flags(format + i + 1, &tmp);
+wid = handle_width(args, format + i + tmp + 1, &tmp);
+prec = handle_precision(args, format + i + tmp + 1, &tmp);
+len = handle_length(format + i + tmp + 1, &tmp);
+f = handle_specifiers(format + i + tmp + 1);
+if (f != NULL)
 {
-	int j;
-	print_modifier modifier[] = {
-				{'c', printf_c},
-				{'s', printf_s},
-				{'i', printf_di},
-				{'d', printf_di},
-				{'b', printf_b},
-				{'o', printf_o},
-				{0, NULL}
-			};
-
-	for (j = 0; modifier[j].func; j++)
-	{
-		if (modifier[j].specifier == *specifier)
-			return (modifier[j].func);
-	}
-
-	return (NULL);
-}
+i += tmp + 1;
+ret += f(args, output, flags, wid, prec, len);
+continue; }
+else if (*(format + i + tmp + 1) == '\0')
+{
+ret = -1;
+break; }}
+ret += _memcpy(output, (format + i), 1);
+i += (len != 0) ? 1 : 0; }
+cleanup(args, output);
+return (ret); }
+/**
+ * _printf - Outputs a formatted string.
+ * @format: Character string to print - may contain directives.
+ *
+ * Return: The number of characters printed.
+ */
+int _printf(const char *format, ...)
+{ buffer_t *output;
+va_list args;
+int ret;
+if (format == NULL)
+return (-1);
+output = init_buffer();
+if (output == NULL)
+return (-1);
+va_start(args, format);
+ret = run_printf(format, args, output);
+return (ret); }
--- main.c
+++ main.c
@@ -1,80 +1,87 @@
+#include <limits.h>
+
+#include <stdio.h>
+
 #include "main.h"
 
-/**
- * main - Entry point
- *
- * Return: 0 on success, error code otherwise
- */
-int main(void)
-{
-	int len, len2;
 
-	len = _printf("%d\n", 1024);
-	len2 = printf("%d\n", 1024);
-	
-	 _printf("%d\n", -1024);
-	 printf("%d\n\n", -1024);
-	 
-	 _printf("%d\n", 0);
-	 printf("%d\n\n", 0);
-	 
-	 _printf("%d\n", INT_MAX);
-	 printf("%d\n\n", INT_MAX);
-	 
-	 _printf("%d\n", INT_MIN);
-	 printf("%d\n\n", INT_MIN);
-	/* _printf("%d", l); 
-	 _printf("%d", l); */
-	 _printf("There is %d bytes in %d KB\n", 1024, 1);
-	 printf("There is %d bytes in %d KB\n\n", 1024, 1);
-	 
-	 _printf("%d - %d = %d\n", 1024, 2048, -1024);
-	 printf("%d - %d = %d\n\n", 1024, 2048, -1024);
-	 
-	 _printf("%d + %d = %d\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
-	 printf("%d + %d = %d\n\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
-	 
-	 _printf("%i\n", -1024);
-	 printf("%i\n\n", -1024);
-	 
-	 _printf("%i\n", 0);
-	 printf("%i\n\n", 0);
-	 
-	 _printf("%i\n", INT_MAX);
-	 printf("%i\n\n", INT_MAX);
-	 
-	 _printf("%i\n", INT_MIN);
-	 printf("%i\n\n", INT_MIN);
-	 
-	/* _printf("%i", l); 
-	 _printf("%i", l); */
-	 _printf("There is %i bytes in %i KB\n", 1024, 1);
-	 printf("There is %i bytes in %i KB\n\n", 1024, 1);
-	 
-	 _printf("%i + %i = %i\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
-	 printf("%i + %i = %i\n\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
-	 
-	 _printf("%d == %i\n", 1024, 1024);
-	 printf("%d == %i\n\n", 1024, 1024);
-	 
-	 _printf("iddi%diddiiddi\n", 1024);
-	  printf("iddi%diddiiddi\n\n", 1024);
-	  
-	 _printf("%d == %i\n", 1024, 1024);
-	 printf("%d == %i\n\n", 1024, 1024);
-	 
-	 _printf("%d\n", 10000);
-	 _printf("%d\n\n", 10000);
-	 
-	 _printf("%i\n", 10000);
-	 _printf("%i\n\n", 10000);
-	 
-	fflush(stdout);
-	if (len != len2)
-	{
-		printf("Lengths differ.\n");
-		fflush(stdout);
-		return (1);
-	}
-	return (0);
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0
+
+ */
+
+int main(void)
+  
+{
+  
+  int len;
+  
+  int len2;
+  
+  unsigned int ui;
+  
+  void *addr;
+  
+
+  
+  len = _printf("Let's try to printf a simple sentence.\n");
+  
+  len2 = printf("Let's try to printf a simple sentence.\n");
+  
+  ui = (unsigned int)INT_MAX + 1024;
+  
+  addr = (void *)0x7ffe637541f0;
+  
+  _printf("Length:[%d, %i]\n", len, len);
+  
+  printf("Length:[%d, %i]\n", len2, len2);
+  
+  _printf("Negative:[%d]\n", -762534);
+  
+  printf("Negative:[%d]\n", -762534);
+  
+  _printf("Unsigned:[%u]\n", ui);
+  
+  printf("Unsigned:[%u]\n", ui);
+  
+  _printf("Unsigned octal:[%o]\n", ui);
+  
+  printf("Unsigned octal:[%o]\n", ui);
+  
+  _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+  
+  printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+  
+  _printf("Character:[%c]\n", 'H');
+  
+  printf("Character:[%c]\n", 'H');
+  
+  _printf("String:[%s]\n", "I am a string !");
+  
+  printf("String:[%s]\n", "I am a string !");
+  
+  _printf("Address:[%p]\n", addr);
+  
+  printf("Address:[%p]\n", addr);
+  
+  len = _printf("Percent:[%%]\n");
+  
+  len2 = printf("Percent:[%%]\n");
+  
+  _printf("Len:[%d]\n", len);
+  
+  printf("Len:[%d]\n", len2);
+  
+  _printf("Unknown:[%r]\n");
+  
+  printf("Unknown:[%r]\n");
+  
+  return (0);
+  
 }
