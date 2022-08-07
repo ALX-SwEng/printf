3a68ec3ae5a7d8b53041085da486fe30b9072fdb
Chere Lemma <chere.lemma@aastu.edu.et>
2022-08-07 16:36:19 +0300

    Printf project commit

--- base_sconverters.c
+++ base_sconverters.c
@@ -16,27 +16,27 @@ unsigned char flags, int wid, int prec);
  */
 unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 unsigned char flags, int wid, int prec)
-{ 
-    int size;
-    char digit, pad = '0';
-    unsigned int ret = 1;
-    for (size = 0; *(base + size);)
-        size++;
-    if (num >= size || num <= -size)
-        ret += convert_sbase(output, num / size, 
-        base, flags, wid - 1, prec - 1);
-    else
-    {
-        for (; prec > 1; prec--, wid--) /* Handle precision */
-            ret += _memcpy(output, &pad, 1);
-            if (NEG_FLAG == 0) /* Handle width */
-            {
-                pad = (ZERO_FLAG == 1) ? '0' : ' ';
-                for (; wid > 1; wid--)
-                    ret += _memcpy(output, &pad, 1); 
-            }
-    }
-    digit = base[(num < 0 ? -1 : 1) * (num % size)];
-    _memcpy(output, &digit, 1);
-    return (ret); 
+{
+	int size;
+	char digit, pad = '0';
+	unsigned int ret = 1;
+	for (size = 0; *(base + size);)
+		size++;
+	if (num >= size || num <= -size)
+		ret += convert_sbase(output, num / size, 
+				base, flags, wid - 1, prec - 1);
+	else
+	{
+		for (; prec > 1; prec--, wid--) /* Handle precision */
+			ret += _memcpy(output, &pad, 1);
+			if (NEG_FLAG == 0) /* Handle width */
+			{
+				pad = (ZERO_FLAG == 1) ? '0' : ' ';
+				for (; wid > 1; wid--)
+					ret += _memcpy(output, &pad, 1); 
+			}
+	}
+	digit = base[(num < 0 ? -1 : 1) * (num % size)];
+	_memcpy(output, &digit, 1);
+	return (ret); 
 }
