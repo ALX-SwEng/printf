ed5eab08b01af92100c5d38518e6b4cfda0918d4
Chere Lemma <chere.lemma@aastu.edu.et>
2022-08-07 19:06:11 +0300

    Printf project update commit

--- handlers.c
+++ handlers.c
@@ -3,7 +3,7 @@
 unsigned char handle_flags(const char *flag, char *index);
 unsigned char handle_length(const char *modifier, char *index);
 int handle_width(va_list args, const char *modifier, char *index);
-int handle_precision(const char *modifier, char *index);
+int handle_precision(va_list args, const char *modifier, char *index);
 unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
 unsigned char, int, int, unsigned char);
 
@@ -106,6 +106,7 @@ int handle_width(va_list args, const char *modifier, char *index)
 /**
  * handle_precision - Matches a precision modifier with
  *                    its corresponding value.
+ * @args: A va_list of arguments.
  * @modifier: A pointer to a potential precision modifier.
  * @index: An index counter for the original format string.
  *
@@ -114,7 +115,7 @@ int handle_width(va_list args, const char *modifier, char *index)
  *         Otherwise - -1.
  */
 
-int handle_precision(const char *modifier, char *index)
+int handle_precision(va_list args, const char *modifier, char *index)
 {
 	int value = 0;
 
