dda86d5f46176fb904eb662ba2dc68198c137bc5
Chere Lemma <chere.lemma@aastu.edu.et>
2022-08-07 21:41:01 +0300

    printf project update

--- convert_numbers.c
+++ convert_numbers.c
@@ -21,7 +21,6 @@ unsigned int convert_o(va_list args, buffer_t *output,
  * @prec: A precision modifier.
  * @len: A length modifier.
  * @output: A buffer_t struct containing a character array.
- *
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int convert_di(va_list args, buffer_t *output,
